e6a7bb8d9d706288d3b4df7269e3e439e937e719
unknown <pflash90@gmail.com>
2022-01-08 15:29:00 +0100

    feat: homepage 90% done

--- components/globals/Nav/NavIndex.jsx
+++ components/globals/Nav/NavIndex.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import styled from './NavIndex.module.css'
 
 // components
 
+// NEXT
+import Image from 'next/image'
+
 const NavIndex = () => {
     return (
-        <nav className="h-16 w-11/12 bg-gray-100">
+        <nav className="h-16 w-10/12 bg-transparent flex justify-between items-center">
+            {/* left */}
+            <div className="h-full w-4/5 flex items-center justify-start space-x-3">
+                {/* logo */}
+                <div className="h-32 w-32 rounded-full bg-transparent flex justify-start items-center -ml-9 mt-5 md:mt-0">
+                    <Image src="/assets/images/logo.png" alt="" width="300" height="300" />
+                </div>
+
+                {/* Menu */}
+                <div className={`h-9 w-full pl-10 border-l border-gray-300 bg-transparent ${styled.rFlex}`}>
+                    <div className="h-full flex justify-start items-center space-x-7">
+                        <div className="text-md font-medium text-gray-500 cursor-pointer border-2 border-gray-500 px-2 rounded-md">Home</div>
+                        <div className="text-md font-normal text-gray-500 cursor-pointer px-2 rounded-md hover:text-black transition duration-700">About</div>
+                        <div className="text-md font-normal text-gray-500 cursor-pointer px-2 rounded-md hover:text-black transition duration-700">Explore</div>
+                        <div className="text-md font-normal text-gray-500 cursor-pointer px-2 rounded-md hover:text-black transition duration-700">How it works</div>
+                    </div>
+                    {/* Search */}
+                    <div className="w-56 bg-red-500 ml-10">
+                        <form className="h-full w-full position relative">
+                            <input className="h-full w-full focus:outline-none pl-7 text-sm text-gray-700 font-medium" type="search" placeholder="Search" />
+                            <i className="la la-search position absolute left-0 top-1/2 -mt-3 text-xl text-gray-500 transform -rotate-90"></i>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            {/* right */}
+            <div className="h-full w-1/5 flex items-center justify-end">
+                {/* hamburger */}
+                <div className="h-full w-6 md:hidden flex flex-col justify-center items-center space-y-1">
+                    <span className="h-[3px] w-full rounded-md bg-gray-800"></span>
+                    <span className="h-[3px] w-10/12 mr-auto rounded-md bg-gray-800"></span>
+                    <span className="h-[3px] w-full rounded-md bg-gray-800"></span>
+                </div>
+
+                {/* Btns */}
+                <div className={`w-46 bg-transparent ${styled.rFlex} flex items-center justify-end space-x-2`}>
+                    <button className="w-12 bg-transparent text-gray-500 font-medium text-md">Login</button>
+                    <button className="w-20 bg-[#9A3432] text-white text-sm py-2 px-3 rounded-md font-medium">Sign Up</button>
+                </div>
+            </div>
         </nav>
     )
 };
--- components/views/Home/Hero/HeroIndex.jsx
+++ components/views/Home/Hero/HeroIndex.jsx
@@ -1,14 +1,58 @@
 import React from 'react'
+import styled from './HeroIndex.module.css';
 
 // components
 
+
+// NEXT
+import Image from 'next/image'
+
 const HeroIndex = () => {
     return (
-        <div className="h-[33rem] w-11/12 flex flex-col md:flex-row justify-center items-center bg-gray-100">
+        <div className="h-auto md:h-[33rem] w-11/12 flex flex-col md:flex-row justify-center items-center bg-transparent">
             {/* Caption */}
-            <div className="h-full w-full md:w-1/3 bg-gray-200"></div>
+            <div className="h-full w-full md:w-1/2 px-1 bg-transparent flex flex-col justify-start md:justify-center items-center md:items-start space-y-7 md:space-y-10 md:pl-32">
+                <h1 className="md:w-10/12 text-4xl md:text-7xl text-center md:text-left font-bold md:leading-none">Never Miss <span className={styled.highlight}>Breakfast</span> Again.</h1>
+                <p className="w-full md:w-7/12 text-sm text-gray-500 text-center md:text-left font-medium">Check out our amazing community of food lovers and get inspired for breakfast.</p>
+                <button className="w-46 bg-[#e85627] text-white py-2 px-5 rounded-md">Find my breakfast</button>
+            </div>
             {/* Mockups */}
-            <div className="h-full w-full md:w-2/3 bg-gray-300"></div>
+            <div className="h-full w-full md:w-1/2 bg-transparent flex items-center justify-center position relative overflow-x-auto md:overflow-x-hidden">
+                {/* First */}
+                <div className="h-full min-w-[450px] md:w-1/2 bg-transparent flex flex-col justify-start space-y-5 pr-5 items-end">
+                    {/* Image */}
+                    <div className="h-72 w-56 rounded-md bg-gray-200 shadow flex jusitfy-center items-center overflow-hidden position relative">
+                        {/* badge */}
+                        <div className="h-5 w-20 rounded-sm px-3 text-center bg-[#219b43] position absolute top-3 right-7 text-white text-xs font-medium z-50">Dessert</div>
+                        <Image src="/assets/images/hero/1.jpg" alt="" width="700" height="900" className="bg-cover" />
+                        {/* Shadow */}
+                        <div className={`h-16 flex items-center font-medium justify-center p-2 position absolute bottom-0 left-0 text-white text-xs ${styled.lBg}`}>
+                            <span className="w-10/12 block">Dessert is best served with friends and family.</span>
+                        </div>
+                    </div>
+                    {/* stats */}
+                    <div className="h-40 w-40 rounded-md bg-[#f8ccbe] flex flex-col items-center justify-center space-y-1">
+                        <h2 className="font-bold text-5xl m-0">4.6K</h2>
+                        <h5 className="font-medium text-lg">Breakfast</h5>
+                    </div>
+                </div>
+                {/* Second */}
+                <div className="h-full md:w-1/2 bg-transparent flex flex-col justify-center space-y-5 pt-16 items-start">
+                    <div className="h-40 w-40 rounded-md bg-[#fdf2e2] flex flex-col items-center justify-center space-y-1">
+                        <h2 className="font-bold text-5xl m-0">500K</h2>
+                        <h5 className="font-medium text-lg">Users</h5>
+                    </div>
+                    <div className="h-72 w-56 rounded-md bg-gray-200 shadow flex jusitfy-center items-center overflow-hidden position relative">
+                        {/* badge */}
+                        <div className="h-5 w-20 rounded-sm px-3 text-center bg-[#219b43] position absolute top-3 right-7 text-white text-xs font-medium z-50">Beverages</div>
+                        <Image src="/assets/images/hero/2.jpg" alt="" width="700" height="900" className="bg-cover" />
+                        {/* Shadow */}
+                        <div className={`h-16 flex items-center font-medium justify-center p-2 position absolute bottom-0 left-0 text-white text-xs ${styled.lBg}`}>
+                            <span className="w-10/12 block">Who doesn't love the morning taste of coffee for real.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
